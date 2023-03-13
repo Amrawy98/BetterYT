9754ca48294611c2a72d28536294e646145e9388
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-03-13 17:25:35 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-slate-600 w-[450px] h-[650px]">
+      <h1>hello banana</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, Plugin } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+import tsconfigPaths from "vite-tsconfig-paths";
+import zip from "rollup-plugin-zip";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    crx({ manifest }),
+    zip() as unknown as Plugin,
+  ],
+});
