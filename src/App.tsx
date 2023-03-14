84de01a6774eec11275ac19e5f865db608e0bea8
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-03-14 14:46:33 +0200

    feat: finish initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import Option from "./components/Option";
+import { OptionConfig } from "./types";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const options: OptionConfig[] = [
+    {
+      label: "Subscriptions search",
+      description: "banana",
+      onToggle: () => console.log("banana"),
+    },
+    {
+      label: "Show queue full time",
+      description: "banana",
+      onToggle: () => console.log("banana"),
+    },
+    {
+      label: "Subscriptions order filters",
+      description: "banana",
+      onToggle: () => console.log("banana"),
+    },
+  ];
 
   return (
-    <div className="bg-slate-600 w-[450px] h-[650px]">
-      <h1>hello banana</h1>
+    <div className="h-[300px] w-[250px] bg-delft-blue p-2">
+      {options.map((option, index) => (
+        <Option
+          label={option.label}
+          description={option.description}
+          onToggle={option.onToggle}
+        />
+      ))}
     </div>
   );
 }
