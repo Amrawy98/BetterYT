80751bbe2f5c0c51e21f1b63d6951730af913b9b
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-03-20 16:30:27 +0200

    feat: persist state of the settings in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
 import Option from "./components/Option";
-import { OptionConfig } from "./types";
+import useSettings from "./hooks/useSettings";
+import { setting } from "./types";
 
 function App() {
-  const options: OptionConfig[] = [
+  const options: setting[] = [
     {
-      label: "Subscriptions search",
-    },
-    {
+      settingKey: "fullQueueTime",
       label: "Show queue full time",
-      onToggle: () => console.log("banana"),
     },
-    {
-      label: "Subscriptions order filters",
-    },
+    // {
+    //   settingKey: "subscriptionsSearch",
+    //   label: "Subscriptions search",
+    // },
+    // {
+    //   settingKey: "subscriptionsOrderFilter",
+    //   label: "Subscriptions order filters",
+    // },
+    // {
+    //   settingKey: "commentsSection",
+    //   label: "Enable Comments Section",
+    // },
+    // {
+    //   settingKey: "filterCommentsSection",
+    //   label: "Enable Comments Section Filter",
+    // },
   ];
+  const [settings, setSettings] = useSettings();
+  console.log("settings", settings);
 
   return (
     <div className="h-[300px] w-[250px] bg-delft-blue p-2">
       {options.map((option, index) => (
         <Option
+          key={option.settingKey}
+          settingKey={option.settingKey}
           label={option.label}
           description={option.description}
-          onToggle={option.onToggle}
+          enabled={settings[option.settingKey as keyof typeof settings]}
+          onToggle={() =>
+            setSettings((prevValue) => ({
+              ...settings,
+              [option.settingKey]:
+                !prevValue[option.settingKey as keyof typeof settings],
+            }))
+          }
         />
       ))}
     </div>
