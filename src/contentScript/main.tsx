75cb97048d3edf7969470a0db55e86c818b49aee
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-13 15:51:04 +0200

    feat: content script main component

--- src/contentScript/main.tsx
+++ src/contentScript/main.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
+function init() {
+  const root = document.createElement("div");
+  root.id = "crx-root";
+  console.log(root);
+  setTimeout(() => {
+    document.querySelectorAll("#publisher-container")[1].appendChild(root);
+    ReactDOM.createRoot(
+      document.querySelector("#crx-root") as HTMLElement
+    ).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }, 100);
+}
 
-const root = document.createElement("div");
-root.id = "crx-root";
-console.log(root);
-document.body.append(root);
-
-ReactDOM.createRoot(document.querySelector("#crx-root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+init();
