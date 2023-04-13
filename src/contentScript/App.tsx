1945d0054d12ed281a3242e0d36e2aadde0a80bc
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-13 15:49:05 +0200

    feat: add content script

--- src/contentScript/App.tsx
+++ src/contentScript/App.tsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+import { useIsFirstRender } from "usehooks-ts";
+import getPlaylistSum from "./utils/getPlaylistSum";
+import Loading from "./Components/Loading";
+
 function App() {
-  return <div className="bg-delft-blue p-2">hello</div>;
+  const [timeNodeList, setTimeNodeList] = useState<NodeListOf<Element>>(
+    document.querySelectorAll("#asdkajsklfdjashdlfaasldfkjhasd")
+  );
+  const isFirst = useIsFirstRender();
+
+  const timeElementsPath =
+    "#items.playlist-items.style-scope.ytd-playlist-panel-renderer ytd-thumbnail-overlay-time-status-renderer span";
+
+  if (isFirst) {
+    setTimeout(() => {
+      const elements = document.querySelectorAll(timeElementsPath);
+      console.log("initial elements", elements.length);
+      setTimeNodeList(elements);
+    }, 1000);
+  }
+
+  const interval = setInterval(() => {
+    const elements = document.querySelectorAll(timeElementsPath);
+    console.log("interval elements", elements.length);
+    if (timeNodeList.length === elements.length) clearInterval(interval);
+    else {
+      setTimeNodeList(elements);
+    }
+  }, 3000);
+  const playListTime = getPlaylistSum(timeNodeList);
+  return (
+    <>
+      {playListTime ? (
+        <div className=" p-2 text-xl leading-4 text-youtube-font">
+          - Total Time: {playListTime}
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
 
 export default App;
