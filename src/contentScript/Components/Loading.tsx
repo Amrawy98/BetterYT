ad730e1fed87e8bd8e68a338dccc4a6f026daec8
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-13 15:49:22 +0200

    feat: add loading component

---
+++ src/contentScript/Components/Loading.tsx
@@ -0,0 +1,22 @@
+const Loading = () => (
+  <div className="flex h-full items-center justify-center p-2">
+    <svg
+      className="h-10 w-10 animate-spin text-youtube-font"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        cx="12"
+        cy="12"
+        r="5.5"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeDasharray="15 95"
+        transform="rotate(-90 12 12)"
+      ></circle>
+    </svg>
+  </div>
+);
+
+export default Loading;
