78f45a7f49bed1b3b0cd143eb095fe790fff97e7
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-14 15:30:15 +0200

    fix: remove setTimeout in contentScript's main

--- src/contentScript/main.tsx
+++ src/contentScript/main.tsx
@@ -6,16 +6,14 @@ function init() {
   const root = document.createElement("div");
   root.id = "crx-root";
   console.log(root);
-  setTimeout(() => {
-    document.querySelectorAll("#publisher-container")[1].appendChild(root);
-    ReactDOM.createRoot(
-      document.querySelector("#crx-root") as HTMLElement
-    ).render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    );
-  }, 100);
+  document.querySelectorAll("#publisher-container")[1].appendChild(root);
+  ReactDOM.createRoot(
+    document.querySelector("#crx-root") as HTMLElement
+  ).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
 
 // Wait for the DOM to finish loading before running the init function
