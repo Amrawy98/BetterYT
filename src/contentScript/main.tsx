6a2154c5c8dad9b31ee4cb30c4ac9b2761c983d5
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-14 15:36:59 +0200

    chore: add todos

--- src/contentScript/main.tsx
+++ src/contentScript/main.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+/**
+ TODO: fix the disappearing youtube icon and the styles that get broken
+ TODO: track user made queues an track when a vide is added
+ */
+
 function init() {
   const root = document.createElement("div");
   root.id = "crx-root";
-  console.log(root);
   document.querySelectorAll("#publisher-container")[1].appendChild(root);
   ReactDOM.createRoot(
     document.querySelector("#crx-root") as HTMLElement
