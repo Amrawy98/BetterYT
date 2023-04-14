8d7fcf3e365d7aaafe2394ea67067f02a213d5bc
Ahmed Amrawy <ahmed.m.amrawy@gmail.com>
2023-04-14 19:49:26 +0200

    fix: change location of content script root component

--- src/contentScript/main.tsx
+++ src/contentScript/main.tsx
@@ -11,14 +11,16 @@ import "./index.css";
 function init() {
   const root = document.createElement("div");
   root.id = "crx-root";
-  document.querySelectorAll("#publisher-container")[1].appendChild(root);
-  ReactDOM.createRoot(
-    document.querySelector("#crx-root") as HTMLElement
-  ).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  setTimeout(() => {
+    document.querySelectorAll("#header-description")[1].appendChild(root);
+    ReactDOM.createRoot(
+      document.querySelector("#crx-root") as HTMLElement
+    ).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }, 1000);
 }
 
 // Wait for the DOM to finish loading before running the init function
